5bdd59f7f6a5f869deeb03f7fbfdb9a5dbaa99c8
yoon4027 <yooooonus@gmail.com>
2022-08-30 20:13:40 +0500

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	const age = Math.abs(
+		new Date(Date.now() - new Date(2009, 3, 7, 10, 50, 0, 0).getTime()).getUTCFullYear() - 1970
+	);
+</script>
+
+<main>
+	<div class="flex md:p-0 h-screen">
+		<div class="relative block text-center items-center m-auto md:flex md:space-x-2">
+			<div class="px-4 sm:px-0 m-auto">
+				<h1 class="font-bold text-5xl">yoon4027</h1>
+				<p class="my-8 prose-sm text-white sm:my-4">
+					{age} year old dickhead who barely even code from the Maldives.
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
